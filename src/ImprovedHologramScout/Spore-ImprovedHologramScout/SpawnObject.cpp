f9be0b672dd4c0ac1c62c05eb28f11b34653ca37
Chocice75 <35960636+VanillaCold@users.noreply.github.com>
2024-11-09 23:29:02 +0000

    push the latest stuff that didn't work

--- src/ImprovedHologramScout/Spore-ImprovedHologramScout/SpawnObject.cpp
+++ src/ImprovedHologramScout/Spore-ImprovedHologramScout/SpawnObject.cpp
@@ -41,9 +41,9 @@ void SpawnObject::ParseLine(const ArgScript::Line& line)
 
 	PropManager.GetPropertyList(thingy.instanceID, thingy.groupID, definition.propList);
 
-	test->SetDefinitionID((int)&definition,0,0);
+	//test->SetDefinitionID((int)&definition,0,0);
 
-	//CALL(Address(0x00c3fde0), int, Args(Simulator::cGameData*, int, size_t, App::Property*), Args(object_cast<Simulator::cGameData>(test), (int)&definition, 0, nullptr));
+	CALL(Address(0x00c3fde0), int, Args(Simulator::cGameData*, int, size_t, App::Property*), Args(object_cast<Simulator::cGameData>(test), (int)&definition, 0, nullptr));
 
 	test->SetScale(test->mScale);
 	test->mbFixed = true;
