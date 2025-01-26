8cbcd0fb0b1e8ef38a2a3c5f649e6cf38c8d7152
Chocice75 <35960636+VanillaCold@users.noreply.github.com>
2025-01-26 01:01:38 +0000

    more work

--- src/ImprovedHologramScout/Spore-ImprovedHologramScout/HologramUIManager.h
+++ src/ImprovedHologramScout/Spore-ImprovedHologramScout/HologramUIManager.h
@@ -7,14 +7,31 @@
 class HologramUIManager 
 	: public Object
 	, public DefaultRefCounted
+	, public App::IUpdatable
 {
+protected:
+	static HologramUIManager* sInstance;
 public:
 	static const uint32_t TYPE = id("HologramUIManager");
 	
 	HologramUIManager();
 	~HologramUIManager();
 
+	void Update() override;
+
+	void OpenUI(bool showAbilities);
+	void UpdateUI();
+	void CloseUI();
+
+	void DeselectUI();
+	static HologramUIManager* Get();
+
+	UILayoutPtr mpLayout;
+
+
 	int AddRef() override;
 	int Release() override;
 	void* Cast(uint32_t type) const override;
+
+
 };
